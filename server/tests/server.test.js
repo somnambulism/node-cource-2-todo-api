29d3c5807bab0d81d8171ed6fdba27483551daab
Semen Vodyannikov <Semen_Vodyannikov@epam.com>
2018-04-27 11:53:40 -0400

    drisnya

---
+++ server/tests/server.test.js
@@ -0,0 +1,24 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+        });
+    });
+});
