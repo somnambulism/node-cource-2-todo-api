19f4bb08363a8335b19230034a3b0fc98671d339
somnambulism <semen.vodyannikov@gmail.com>
2018-04-28 12:46:26 +0300

    Test POST /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,10 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         var text = 'Test todo text';
@@ -19,6 +23,29 @@ describe('POST /todos', () => {
             if (err) {
                 return done(err);
             }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
         });
     });
 });
